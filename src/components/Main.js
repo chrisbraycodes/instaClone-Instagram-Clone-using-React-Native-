be78a3679d6400eb5202188949b454f0bbed3965
Christopher Bray <comp.chris@icloud.com>
2024-08-17 21:17:08 -0700

    Added navigation buttons and tabs
    added navigation buttons and tabs

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,8 +3,11 @@ import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchUser } from '../redux/actions/index';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
 import FeedScreen from './main/Feed'
+import AddScreen from './main/Add'
+import ProfileScreen from './main/Profile'
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
@@ -18,7 +21,24 @@ class Main extends Component {
   render() {
     return (
       <Tab.Navigator>
-        <Tab.Screen name="Feed" component={FeedScreen} />
+        <Tab.Screen name="Feed" component={FeedScreen}
+        options={{
+          tabBarIcon:({ color, size }) => (
+            <MaterialCommunityIcons name = "home" color={color} size = {26}/>
+          )
+        }}/>
+        <Tab.Screen name="Add" component={AddScreen}
+        options={{
+          tabBarIcon:({ color, size }) => (
+            <MaterialCommunityIcons name = "plus-box" color={color} size = {26}/>
+          )
+        }}/>
+        <Tab.Screen name="Profile" component={ProfileScreen}
+        options={{
+          tabBarIcon:({ color, size }) => (
+            <MaterialCommunityIcons name = "account-circle" color={color} size = {26}/>
+          )
+        }}/>
       </Tab.Navigator>
     );
   }
